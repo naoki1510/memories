23922e0cbd9e86f3b52ebef0fc79dc444c9bf878
naoki1510 <naoki1510@shallweband.net>
2020-02-29 00:28:39 +0900

    html仮作成

--- public/index.php
+++ public/index.php
@@ -1,11 +1,43 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>memories</title>
 </head>
+
 <body>
-    <h1>Hello World!</h1>
+    <div class="header-container">
+        <div class="header">
+            <header>
+                <div class="site-title">
+                    <h1>Hello World!</h1>
+                </div>
+            </header>
+        </div>
+    </div>
+    <div class="main">
+        <div class="content">
+            <div class="list">
+                <div class="card">
+                    <div class="card-title">card-title</div>
+                    <div class="card-content">
+                        <div class="text">文字</div>
+                        <div class="img">
+                            <img src="/" alt="ここに画像 一枚に合成できるといいけどhtmlで並べるのも可">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="sidebar">
+        <!-- いるかこれ？ -->
+    </div>
+    <div class="footer">
+        <footer></footer>
+    </div>
 </body>
+
 </html>
